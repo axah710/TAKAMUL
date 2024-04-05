f5222e232411a59de5a6f16bb08e78e910f4a0b6
axah710 <ahmed36085361@gmail.com>
2024-04-05 02:14:41 +0200

    added some comments

--- lib/helper/simple_bloc_observer.dart
+++ lib/helper/simple_bloc_observer.dart
@@ -9,6 +9,11 @@ class SimpleBlocObserver extends BlocObserver {
       print("change=$change");
     }
   }
+  // This method is triggered whenever there is a change in the state of
+  // any BlocBase (which includes both Blocs and Cubits). The change is
+  //passed as an argument, and if the application is in debug mode
+  //(kDebugMode is true), it prints the change to the console.
+  //This is useful for debugging state changes.
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
@@ -17,6 +22,12 @@ class SimpleBlocObserver extends BlocObserver {
       print("transition=$transition");
     }
   }
+  // Specifically for Bloc instances (not Cubits), this method is called
+  //during a state transition, triggered by an event.
+  //A Transition object, which includes the current state,
+  // the event, and the next state, is provided. If in debug mode,
+  //this transition is printed to the console. This helps in understanding
+  // the flow of events and state changes within a Bloc.
 
   @override
   void onClose(BlocBase bloc) {
@@ -25,6 +36,9 @@ class SimpleBlocObserver extends BlocObserver {
       print("close=$bloc");
     }
   }
+  // This method is called just before a BlocBase (Bloc or Cubit) is closed.
+  // It's a good place to perform cleanup or logging. In debug mode,
+  // it prints the instance of the Bloc or Cubit that is being closed.
 
   @override
   void onCreate(BlocBase bloc) {
@@ -33,4 +47,7 @@ class SimpleBlocObserver extends BlocObserver {
       print("create=$bloc");
     }
   }
+  // Opposite to onClose, this method is invoked when a
+  // BlocBase instance is created. It allows for initial setup or logging.
+  // In debug mode, it prints the created instance.
 }
